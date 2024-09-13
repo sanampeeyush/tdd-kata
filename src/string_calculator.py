56ae86adf02aff2ca40a39afccd1dd806202dedf
Peeyush Sanam <peeyush.sanam@gmail.com>
2024-09-13 19:10:12 +0530

    Testing single and two numbers

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -1,3 +1,10 @@
 class StringCalculator:
+    """
+    String calculator base class
+    """
+
     def add(self, numbers: str) -> int:
-        return 0
+        if numbers == "":
+            return 0
+        num_list = list(map(int, numbers.split(",")))
+        return sum(num_list)
--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -6,3 +6,11 @@ class TestStringCalculator(unittest.TestCase):
     def test_empty_string(self):
         calc = StringCalculator()
         self.assertEqual(calc.add(""), 0)
+
+    def test_single_number(self):
+        calc = StringCalculator()
+        self.assertEqual(calc.add("1"), 1)
+
+    def test_two_numbers(self):
+        calc = StringCalculator()
+        self.assertEqual(calc.add("1,2"), 3)
