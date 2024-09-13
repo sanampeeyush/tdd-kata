a1635215b2063c9df2d80eb951fa5a4b5ff0394e
Peeyush Sanam <peeyush.sanam@gmail.com>
2024-09-13 19:14:18 +0530

    negative number addition logic

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -11,11 +11,14 @@ class StringCalculator:
             return 0
 
         delimiter = ",|\n"
-
         if numbers.startswith("//"):
             parts = numbers.split("\n", 1)
             delimiter = re.escape(parts[0][2:])
             numbers = parts[1]
 
-        num_list = re.split(delimiter, numbers)
-        return sum(map(int, num_list))
+        num_list = list(map(int, re.split(delimiter, numbers)))
+        negatives = [num for num in num_list if num < 0]
+        if negatives:
+            raise ValueError(f"negatives not allowed: {negatives}")
+
+        return sum(num_list)
