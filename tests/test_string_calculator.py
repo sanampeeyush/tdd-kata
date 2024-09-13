0191d074c271e9ea842b3eb8902fa2518d96fc89
Peeyush Sanam <peeyush.sanam@gmail.com>
2024-09-13 20:02:55 +0530

    final touch ups

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -3,63 +3,53 @@ from src.string_calculator import StringCalculator
 
 
 class TestStringCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = StringCalculator()
+
     def test_empty_string(self):
-        calc = StringCalculator()
-        self.assertEqual(calc.add(""), 0)
+        self.assertEqual(self.calc.add(""), 0)
 
     def test_single_number(self):
-        calc = StringCalculator()
-        self.assertEqual(calc.add("1"), 1)
+        self.assertEqual(self.calc.add("1"), 1)
 
     def test_two_numbers(self):
-        calc = StringCalculator()
-        self.assertEqual(calc.add("1,2"), 3)
+        self.assertEqual(self.calc.add("1,2"), 3)
 
     def test_multiple_numbers(self):
-        calc = StringCalculator()
-        self.assertEqual(calc.add("1,2,3,4"), 10)
+        self.assertEqual(self.calc.add("1,2,3,4"), 10)
 
     def test_newlines_as_delimiters(self):
-        calc = StringCalculator()
-        self.assertEqual(calc.add("1\n2,3"), 6)
+        self.assertEqual(self.calc.add("1\n2,3"), 6)
 
     def test_custom_delimiter(self):
-        calc = StringCalculator()
-        self.assertEqual(calc.add("//;\n1;2"), 3)
+        self.assertEqual(self.calc.add("//;\n1;2"), 3)
+
+    def test_multiple_custom_delimiters(self):
+        self.assertEqual(self.calc.add("//[*][%]\n1*2%3"), 6)
+
+    def test_long_custom_delimiters(self):
+        self.assertEqual(self.calc.add("//[***]\n1***2***3"), 6)
+
+    def test_multiple_long_custom_delimiters(self):
+        self.assertEqual(self.calc.add("//[**][%%]\n1**2%%3"), 6)
 
     def test_negative_numbers(self):
-        calc = StringCalculator()
         with self.assertRaises(ValueError) as cm:
-            calc.add("1,-2,3")
+            self.calc.add("1,-2,3")
         self.assertEqual(str(cm.exception), "negatives not allowed: [-2]")
 
     def test_multiple_negative_numbers(self):
-        calc = StringCalculator()
         with self.assertRaises(ValueError) as cm:
-            calc.add("1,-2,-3")
+            self.calc.add("1,-2,-3")
         self.assertEqual(str(cm.exception), "negatives not allowed: [-2, -3]")
 
-    def test_called_count(self):
-        calc = StringCalculator()
-        calc.add("1,2")
-        calc.add("3,4")
-        self.assertEqual(calc.get_called_count(), 2)
-
     def test_ignore_large_numbers(self):
-        calc = StringCalculator()
-        self.assertEqual(calc.add("2,1001"), 2)
+        self.assertEqual(self.calc.add("2,1001,6"), 8)
 
-    def test_multiple_delimiters(self):
-        calc = StringCalculator()
-        self.assertEqual(calc.add("//[*][%]\n1*2%3"), 6)
-
-    def test_longer_delimiters(self):
-        calc = StringCalculator()
-        self.assertEqual(calc.add("//[***]\n1***2***3"), 6)
-
-    def test_multiple_long_delimiters(self):
-        calc = StringCalculator()
-        self.assertEqual(calc.add("//[**][%%]\n1**2%%3"), 6)
+    def test_called_count(self):
+        self.calc.add("1,2")
+        self.calc.add("3,4")
+        self.assertEqual(self.calc.get_called_count(), 2)
 
 
 if __name__ == "__main__":
